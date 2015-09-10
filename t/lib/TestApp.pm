05dca3354c2bafe0aea1da63178162d7937aa4cd
Ruben Amortegui <ruben@yoursole.com>
2015-09-10 09:46:53 -0600

    Load the plugin successfull

--- t/lib/TestApp.pm
+++ t/lib/TestApp.pm
@@ -2,8 +2,19 @@ package t::lib::TestApp;
 
 use Dancer2;
 use Dancer2::Plugin::DBIC qw(schema resultset);
+use Dancer2::Plugin::ECommerce;
 get '/' => sub {
   'Hello World'
 };
+
+get '/cart/new/:cart_new?' => sub {
+  my ($cart_name) = param('cart_new'); 
+ 
+  my $cart = cart($cart_name); 
+  $cart->{'name'};
+
+};
+
+
 
 1;
