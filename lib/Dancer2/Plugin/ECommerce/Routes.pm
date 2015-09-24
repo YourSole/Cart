d4954af0800bf07679ea946507491a673562d14d
Ruben Amortegui <ruben@yoursole.com>
2015-09-24 14:28:22 -0600

    Add Info to cart page - Add quantity and price - Add subtotal - Add links to naviagate between cart and products - Add link to clear the cart. -

--- lib/Dancer2/Plugin/ECommerce/Routes.pm
+++ lib/Dancer2/Plugin/ECommerce/Routes.pm
@@ -1,15 +1,17 @@
 my $product_name = undef;
 my $product_pk = undef;
 
-$product_name = $settings->{product_name} || 'EcProduct';
-$product_pk = $settings->{product_pk} || 'sku';
+
+$product_name = app->config->{'plugins'}->{'ECommerce::Cart'}->{product_name} || 'EcProduct';
+$product_pk = app->config->{'plugins'}->{'ECommerce::Cart'}->{product_pk} || 'sku';
+$product_price_f = app->config->{'plugins'}->{'ECommerce::Cart'}->{product_price_f} || 'sellprice';
 
 get '/products' => sub {
   my $page = "<table><tr><th>Sku</th><th>Action</th></tr>";
   my @products = schema->resultset($product_name)->all; 
   map {
-    $page .= "<tr><td>".$_->sku."</td><td><form method='post' action='add'>
-    <input type='hidden' name='sku' value='".$_->sku."'>
+    $page .= "<tr><td>".$_->$product_pk."</td><td><form method='post' action='cart/add'>
+    <input type='hidden' name='sku' value='".$_->$product_pk."'>
     <input type='hidden' name='quantity' value='1'> 
     <input type='submit' value = 'Add'>
     </form></td></tr>\n";
@@ -25,24 +27,38 @@ post '/cart/add' => sub {
 };
 
 get '/cart' => sub {
-  my $page = "<table><tr><th>SKU</th><th></th><th>Quantity</th><th></th></th>\n";
   my $products = products;
-  map{
-    $page .= "<tr><td>".$_->{sku}."</td><td><form method='post' action='add'>
-    <input type='hidden' name='sku' value='".$_->{sku}."'>
-    <input type='hidden' name='quantity' value='-1'> 
-    <input type='submit' value = '-1'>
-    </form></td><td>". $_->{quantity} ."</td><td><form method='post' action='add'>
-    <input type='hidden' name='sku' value='".$_->{sku}."'>
-    <input type='hidden' name='quantity' value='1'> 
-    <input type='submit' value = '+1'>
-    </form></td></tr>\n";
-  } @{$products};
-  $page .= "</table>"; 
+  my $page = "";
+
+  if (@{$products} > 0 ) {
+    $page .= "<a href='products'> Continue shooping. </a>";
+    $page .= "<table><tr><th>SKU</th><th></th><th>Quantity</th><th></th><th>Price</th></tr>\n";
+    map{
+      $page .= "<tr><td>".$_->{$product_pk}."</td><td><form method='post' action='cart/add'>
+      <input type='hidden' name='sku' value='".$_->{$product_pk}."'>
+      <input type='hidden' name='quantity' value='-1'>
+      <input type='submit' value = '-1'>
+      </form></td><td>". $_->{quantity} ."</td><td>
+      <form method='post' action='cart/add'>
+        <input type='hidden' name='sku' value='".$_->{$product_pk}."'>
+        <input type='hidden' name='quantity' value='1'>
+        <input type='submit' value = '+1'>
+      </form></td><td>".$_->{price}."</td</tr>\n";
+    } @{$products};
+    $page .= "<tr><td colspan=4>Subtotal</td><td>".subtotal."</td></tr>";
+    $page .= "</table>";
+    $page .= "<a href='cart/clear'> Clear your cart. </a>";
+  }
+  else{
+    $page .= "Your cart is empty. <a href='products'> Continue shooping. </a>";
+  }
   $page;
 };
 
-
+get '/cart/clear' => sub {
+  clear_cart;
+  redirect '/cart';
+};
 
 1;
 
